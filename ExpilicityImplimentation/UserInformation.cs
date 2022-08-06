47c5eec7350f57798c10e2e1f9b496797a1e4646
SinaJabermoradi <sinajabermoradi313@gmail.com>
2022-08-06 18:37:00 +0430

    Create User Information Class

---
+++ ExpilicityImplimentation/UserInformation.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpilicityImplimentation
+{
+    public class UserInformation
+    {
+
+        #region Attributes
+
+        private string _userFullName;
+        private long _id;
+        private string _messageToUser;
+        private Roles _role;
+
+        #endregion
+
+        #region Behaviors
+
+        #region Getter & Setter
+
+
+        public string UserFullName
+        {
+            get { return _userFullName; }
+            set { _userFullName = value; }
+        }
+
+        public long Id
+        {
+            get { return _id; }
+            set { _id = value; }
+        }
+
+        public string MessageToUser
+        {
+            get { return _messageToUser; }
+            set { _messageToUser = value; }
+        }
+
+        public Roles Role
+        {
+            get { return _role; }
+            set { _role = value; }
+        }
+
+
+        #endregion
+
+        #endregion
+
+    }
+}
