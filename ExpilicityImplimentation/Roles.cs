37cfff1ecfbf7bf62fb1b30782dc2f9e5de58dfb
SinaJabermoradi <sinajabermoradi313@gmail.com>
2022-08-06 18:37:23 +0430

    Create Roles

---
+++ ExpilicityImplimentation/Roles.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpilicityImplimentation
+{
+    public enum Roles
+    {
+        Manager = 1,
+        Clerk = 2,
+        Costumer = 3
+    }
+}
