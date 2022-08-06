5330d6710d1a1f0aa60407dd2502f496fab05034
SinaJabermoradi <sinajabermoradi313@gmail.com>
2022-08-06 18:18:13 +0430

    Add project files.

---
+++ ExpilicityImplimentation/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ExpilicityImplimentation
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
