6b7827a0ad3168b5c1a95ff4088636c93a71c27b
SinaJabermoradi <sinajabermoradi313@gmail.com>
2022-08-06 18:36:28 +0430

    Create ISender

---
+++ ExpilicityImplimentation/ISender.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ExpilicityImplimentation
+{
+    public interface ISender
+    {
+        public void Sender(UserInformation information);
+    
+    }
+}
